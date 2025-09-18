3505bd32761b78f1132f5fbd53704ff2461e383a
moataz kamal <moatazkmal@gmail.com>
2025-09-18 23:00:21 +0300

    middleware

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,17 @@
+import { authoptions } from "@/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function Cart() {
+export default async function Cart() {
+
+
+  // let session = await getServerSession(authoptions);
+
+  // if(!session) {
+  //   redirect('/login');
+  // }
+
+
   return <div>Cart</div>;
 }
